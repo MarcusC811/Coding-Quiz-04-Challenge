d4722930225245e4d1f9a475cb84561ac49bca8d
OVOMARK <mcncstatefan@icloud.com>
2022-09-20 15:47:41 -0400

    function structure for question display

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,41 @@
-function init() {
+var startButton = document.getElementById("startButton");
+var correctAnswers = localStorage.getItem("rightAnswer");
+var quizSection = document.getElementById("quiz");
+var questions = [
+    {
+        question: 'What 9 + 10?',
+        Answers: {
+        A: '21',
+        B: '11', 
+        C: '14',
+        D: '19' 
+        },
+    correctAnswer: 'D'
     
+    },
+
+    {
+        question: 'What 9 + 10?',
+        Answers: {
+        A: '21',
+        B: '11', 
+        C: '14',
+        D: '19' 
+        },
+    correctAnswer: 'D'
+    
+    }
+];
+console.log(typeof questions);
+
+function showQuestions() {
+    function generateQuestions(){
+
+    }
+
+    function showResults(){
+
+    }
 }
+
+startButton.addEventListener("click", showQuestions);
